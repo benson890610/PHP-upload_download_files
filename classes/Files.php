771e2ea7425aa0fa0f98ac7326e3de1a93845059
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-10-14 13:39:11 +0200

    Created: Files class

---
+++ classes/Files.php
@@ -0,0 +1,13 @@
+<?php
+
+require '../interfaces/FilesInterface.php';
+
+class Files implements FilesInterface {
+
+    public function display() {}
+
+    public function upload() {}
+
+    public function download() {}
+
+}
