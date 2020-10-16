e7c3f6b2674f0c298cbd824d1972db6c43d5b95f
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-10-16 09:57:05 +0200

    Modified proccess.php, new files included

--- proccess.php
+++ proccess.php
@@ -1,7 +1,8 @@
 <?php
     ini_set('display_errors', 'on');
     session_start();
-
+    require 'interfaces/ClassInterfaces.php';
+    require 'classes/Validation.php';
     require 'classes/Files.php';
     
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
