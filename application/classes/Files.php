5a4e3049598cad9f0dc60030cd40c7edaceef8a4
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-10-19 15:31:02 +0200

    Modified: class Files finished

--- application/classes/Files.php
+++ application/classes/Files.php
@@ -1,39 +1,31 @@
 <?php
 
-require 'File.php';
-
 class Files implements FilesInterface {
 
     private $db;
     private $files = [];
     private $maxUploadFiles = 5;
 
-    public function __construct() {
-        $this->db = new DBmodel;
-    }
-
     public function createUploadFiles() {
+        $validate    = new Validation;
         $this->files = $_FILES['files'];
-        $validate = new Validation;
-
         // Client has not choosed an image
         if($validate->notSubmited($this->files)) {
             $validate->storeMessage('error', 'Please choose a file for upload');
         } else {
-
             // Check if client has submited more than 5 files
             $totalFilesForUpload = count($this->files['name']);
+            
             if($totalFilesForUpload > $this->maxUploadFiles) {
                 $validate->storeMessage('error', 'Maximum ' . $this->maxUploadFiles . ' files are allowed for upload');
             }
-
             // Restrict to maximum 5 files for upload
-            $totalFiles = $this->getTotalFiles();
-
+            $totalFiles = $totalFilesForUpload > $this->maxUploadFiles ? $this->maxUploadFiles : $totalFilesForUpload;
             // Save file data to database, upload actual file to files directory
             for($i = 0; $i < $totalFiles; $i++) {
 
                 $file = $this->createFile($i);
+                
                 if($validate->unauthorize($file)) {
                     $validate->storeMessage('error', $file->__get('name') . ' is not allowed for upload');
                 } elseif($validate->bigsize($file)) {
@@ -43,16 +35,15 @@ class Files implements FilesInterface {
                     $file->upload();
                     $validate->storeMessage('success', $file->__get('name') . ' has been uploaded');
                 }
-
             }
         }
-    
         $messages = $validate->getMessages();
         session($messages);
         redirect('index');
     }
 
-    public function displayLinks() {
+    public function showFiles() {
+        $this->db = new DBmodel;
         $files = $this->db->all();
         $htmlFiles = [];
         $htmlFiles = array_map(function($file){ return "<li class='list-group-item'><a href='proccess.php?file=". $file['id'] ."'>". $file['name'] ."</a></li>"; }, $files);
@@ -64,7 +55,7 @@ class Files implements FilesInterface {
         $file = new File;
 
         $filename = pathinfo($this->files['name'][$i], PATHINFO_FILENAME);
-        $ext = pathinfo($this->files['name'][$i], PATHINFO_EXTENSION);
+        $ext      = pathinfo($this->files['name'][$i], PATHINFO_EXTENSION);
 
         $file->__set('name',     $filename . '_' . time() . '.' . $ext);
         $file->__set('tmp_name', $this->files['tmp_name'][$i]);
@@ -72,15 +63,5 @@ class Files implements FilesInterface {
         $file->__set('size',     $this->files['size'][$i]);
 
         return $file;
-
     }
-
-    private function getTotalFiles() {
-
-        $files = count($this->files['name']);
-        $files = ($files > $this->maxUploadFiles) ? $this->maxUploadFiles : $files;
-
-        return $files;
-    }
-
 }
