36d58211bd2cd47eccdc09b13da7607c60b54c7b
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-10-19 16:09:50 +0200

    Project completed

--- application/classes/File.php
+++ application/classes/File.php
@@ -1,11 +1,10 @@
 <?php
 
-    require "DBmodel.php";
-
     class File implements FileInterface {
 
         private $db;
 
+        private $id;
         private $name;
         private $tmp_name;
         private $type;
@@ -14,18 +13,36 @@
         private $maxSize = 3000000;
 
         public function __construct() {
-
             $this->db = new DBmodel;
-
-        }
+        } 
 
         public function save() {
             $this->size = (int) $this->size;
-            $this->db->save($this->name, $this->type, $this->size);
+            $this->db->persist($this->name, $this->type, $this->size);
         }
 
         public function upload() {
-            move_uploaded_file($this->tmp_name, "application/files/" . $this->name);
+            move_uploaded_file($this->tmp_name, "files/" . $this->name);
+        }
+
+        public function download() {
+
+            $id = htmlspecialchars($_GET['file']);
+            $id = (int) $id;
+            
+            $file = $this->db->findSingle($id);
+
+            $destination = "files/";
+            
+            header('Content-Description: File Transfer');
+            header('Content-Type:' . $file['type']);
+            header('Content-Disposition: attachment;filename=' . $file['name']);
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Content-Length: ' . $file['size']);
+            flush();
+            readfile($destination . $file['name']);
+
         }
 
         public function __set($property, $value) {
