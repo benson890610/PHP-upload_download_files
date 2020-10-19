04d5ebc9e8c7f3224f99454365f163ffdd9d7116
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-10-19 15:34:20 +0200

    Finished proccess requests page

--- proccess.php
+++ proccess.php
@@ -2,15 +2,24 @@
     ini_set('display_errors', 'on');
     session_start();
     require 'application/interfaces/ClassInterfaces.php';
-    require 'application/classes/Validation.php';
-    require 'application/classes/Files.php';
-    require 'helpers.php';
+    require 'application/helpers.php';
+    spl_autoload_register(function($className){
+        if(file_exists('application/classes/' . $className . '.php')){
+            require 'application/classes/' . $className . '.php';
+        }
+    });
     
+    // Upload commited
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $files = new Files;
         $files->createUploadFiles();
+    // Download commited
     } else {
 
-        // Download files
-
+        if(isset($_GET['file']) && !empty($_GET['file'] && is_numeric($_GET['file']))) {
+            $file = new File;
+            $file->download();
+        } else {
+            redirect('index');
+        }
     }
